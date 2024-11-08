873e38054ab48d50aab08f8c458c6c6efe0527f5
yogita-gkmit <yogita@gkmit.co>
2024-11-08 17:44:50 +0530

    feat: [wc-6] add nodemailer setup

---
+++ src/utils/email.util.js
@@ -0,0 +1,12 @@
+const nodemailer = require('nodemailer');
+
+let transporter = nodemailer.createTransport({
+  host: process.env.MAIL_HOST,
+  port: 587,
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
+  },
+});
+
+module.exports = { transporter };
