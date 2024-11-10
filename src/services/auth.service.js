4b4ca16cf3ad0aade36dc03f03097f5455c46f68
yogita-gkmit <yogita@gkmit.co>
2024-11-10 17:01:48 +0530

    feat:[wc-7] sent otp to response

--- src/services/auth.service.js
+++ src/services/auth.service.js
@@ -43,7 +43,7 @@ async function sendOtp(email) {
 		}
 	});
 
-	return { message: `otp sent successfully ${otp}` };
+	return { message: 'otp sent successfully', otp };
 }
 
 async function verifyOtp(email, otp) {
