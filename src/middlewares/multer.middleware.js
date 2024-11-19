f3d68743043b60f07d52fa6f8cbb5c20d01fca6c
yogita-gkmit <yogita@gkmit.co>
2024-11-19 16:42:18 +0530

    fix:[wc-12] fix files while writing test cases

--- src/middlewares/multer.middleware.js
+++ src/middlewares/multer.middleware.js
@@ -2,10 +2,10 @@ const multer = require('multer');
 
 const storage = multer.diskStorage({
   destination: function (req, file, cb) {
-    cb(null, './src/uploads'); // Uploads will be stored in the 'uploads/' directory
+    cb(null, './src/uploads');
   },
   filename: function (req, file, cb) {
-    cb(null, file.originalname); // Use the original file name
+    cb(null, file.originalname);
   },
 });
 
